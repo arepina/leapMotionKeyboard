1950daa732e3234f43076d92275b232ee67ffccd
arepina <anastasiya.repina2012@yandex.ru>
2019-08-25 14:27:22 +0300

    Added keyboard

---
+++ Assets/Layouts/Keyboard.cs
@@ -0,0 +1,18 @@
+﻿/*namespace LeapMotionKeyboard.Layouts
+{
+    public class Keyboard : Layout
+    {
+
+        public Keyboard()
+        {
+            row1Keys = new string[] { "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=" };
+            row1Shift = new string[] { "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+" };
+            row2Keys = new string[] { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]" };
+            row2Shift = new string[] { "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}" };
+            row3Keys = new string[] { "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "\\" };
+            row3Shift = new string[] { "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "\"", "|" };
+            row4Keys = new string[] { "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/" };
+            row4Shift = new string[] { "|", "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?" };
+        }
+    }
+}*/
