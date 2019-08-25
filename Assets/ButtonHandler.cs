e213366a5480d011ae910f4446829f90f5702f73
arepina <anastasiya.repina2012@yandex.ru>
2019-08-25 18:33:34 +0300

    Started adding the onClick event

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Leap.Unity.Interaction;
+using System;
+
+[RequireComponent(typeof(InteractionBehaviour))]
+public class ButtonHandler : MonoBehaviour
+{
+    private InteractionBehaviour _intObj;
+
+    void Start()
+    {
+        Debug.Log("Start");
+        _intObj = GetComponent<InteractionBehaviour>();
+        _intObj.OnHoverBegin += onHoverBegin;
+        _intObj.OnPerControllerHoverBegin += OnPerControllerHoverBegin;
+    }
+
+    private void OnPerControllerHoverBegin(InteractionController obj)
+    {
+        Debug.Log((InteractionController)obj.intHand);
+    }
+
+    private void onHoverBegin()
+    {
+        Debug.Log("onHoverBegin");
+    }
+
+    public void setText(string text)
+    {
+        Debug.Log("setText");
+        Text txt = transform.Find("Text").GetComponent<Text>();
+        txt.text = text;
+    }
+}
